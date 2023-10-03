3bc863447b667892d3bf19abaf2908759c63ffe1
wikalsnri <wikalsnri@gmail.com>
2023-10-03 16:03:06 +0700

    Ubah menyesuaikan tugas hari 1

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,8 +12,27 @@
         <br>
         <label for="last_name">Last Name:</label>
         <input type="text" name="last_name" required><br>
+        <br><br>
+        <label>Gender :</label>
+        <br><br>
+        <input type="radio">Male<br>
+        <input type="radio">Female<br>
+        <input type="radio">Other<br><br>
+        <label for="pilih-negara">Nationality :</label><br><br>
+        <select id="pilih-negara">
+            <option value="Indonesia">Indonesia</option>
+            <option value="Malaysia">Malaysia</option>
+            <option value="Vietnam">Vietnam</option>
+            <option value="Singapore">Singapore</option>
+        </select><br><br>
+        <label>Language Spoken :</label><br><br>
+        <input type="checkbox">Bahasa Indonesia<br>
+        <input type="checkbox">English<br>
+        <input type="checkbox">Other<br><br>
 
+        <label>Bio :</label><br><br>
+        <textarea name="message" id="" cols="30" rows="10"></textarea><br><br>
         <button type="submit">Submit</button>
     </form>
 </body>
-</html>
+</html> 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <title>Welcome ~ WikaSITE</title>
 </head>
 <body>
-    <h1>Welcome to WikaSITE, {{ $firstName }} {{ $lastName }}!</h1>
+    <h1>Welcome to Garuda Cyber Institute, {{ $firstName }} {{ $lastName }}!</h1>
+    <p>Terima  Kasih Telah Bergabung Bersama Garuda Cyber Institute</p>
     <a href="/">Balik ke Home Nih!</a>
 </body>
 </html>
